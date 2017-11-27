0628da9248eac4f2a11764041d80baada0f67850
Drew Carter <drewcarter0@gmail.com>
2017-11-27 14:48:15 -0500

    Add files via upload
    added classes

---
+++ InvProj/InventoryItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    class InventoryItem
+    {
+        private string UpcCode;
+        private string description;
+        private int quantityOnHand;
+        private int reorderPoint;
+        private int reorderQuantity;
+        private int quantityOnOrder;
+
+        public string UpcCode1 { get => UpcCode; set => UpcCode = value; }
+        public string Description { get => description; set => description = value; }
+        public int QuantityOnHand { get => quantityOnHand; set => quantityOnHand = value; }
+        public int ReorderPoint { get => reorderPoint; set => reorderPoint = value; }
+        public int ReorderQuantity { get => reorderQuantity; set => reorderQuantity = value; }
+        public int QuantityOnOrder { get => quantityOnOrder; set => quantityOnOrder = value; }
+
+        public InventoryItem()
+        {
+            UpcCode = "fff";
+            description = "fff";
+            quantityOnHand = 0;
+            reorderPoint = 0;
+            reorderQuantity = 0;
+            quantityOnOrder = 0;
+        }
+
+       
+    }
+}
---
+++ InvProj/Vendor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    class Vendor
+    {
+        private string vendorID;
+        private string companyName;
+        private string agentName;
+        private string phoneNumber;
+
+        public string VendorID { get => vendorID; set => vendorID = value; }
+        public string CompanyName { get => companyName; set => companyName = value; }
+        public string AgentName { get => agentName; set => agentName = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+
+        public Vendor()
+        {
+            vendorID = "fdsa";
+            companyName = "asdf";
+            agentName = "steve";
+            phoneNumber = "111-123-4567";
+        }
+    }
+}
